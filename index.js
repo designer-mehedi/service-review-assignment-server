f57d9ca475dba514279cb64930e27f0d8af28247
designermehedi <mehedihasan80912@gmail.com>
2022-11-15 19:22:24 +0600

    server updated

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config(); 
 const app = express();
@@ -24,11 +23,6 @@ const run = async() => {
         const serviceCollection = client.db('serviceReview').collection('services'); 
         const reviewCollection = client.db("serviceReview").collection('reviews');
 
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
-            res.send({token});  
-        })
 
         app.get('/services', async(req, res) => {
             const query = {};
